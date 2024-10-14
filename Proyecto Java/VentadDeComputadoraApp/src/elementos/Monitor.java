75a19df31d932ee0b12d265fb362d8718b05d0b0
CodeTornadoStorms <demyu369@gmail.com>
2024-10-14 18:32:37 -0500

    Clase Monitor

---
+++ Proyecto Java/VentadDeComputadoraApp/src/elementos/Monitor.java
@@ -0,0 +1,53 @@
+package elementos;
+
+public class Monitor {
+	private static int contadorMonitores = 0;
+
+	private int idMonitor;
+	private String marca;
+	private double tamanio;
+
+	public Monitor() {
+
+	}
+
+	public Monitor(int idMonitor, String marca, double tamanio) {
+
+		this.idMonitor = idMonitor;
+		this.marca = marca;
+		this.tamanio = tamanio;
+	}
+
+	public int getIdMonitor() {
+		return idMonitor;
+	}
+
+	public void setIdMonitor(int idMonitor) {
+		this.idMonitor = idMonitor;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public double getTamanio() {
+		return tamanio;
+	}
+
+	public void setTamanio(double tamanio) {
+		this.tamanio = tamanio;
+	}
+
+	public static int getContadorMonitores() {
+		return contadorMonitores;
+	}
+
+	@Override
+	public String toString() {
+		return "Monitor [idMonitor=" + idMonitor + ", marca=" + marca + ", tamanio=" + tamanio + "]";
+	}
+}
